0ed0b59fbf4ba3cb123d84ae3b3b8eb3a5136ced
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-05-22 19:40:12 +0200

    Changed behaviour of global taints for returns
    - A global is tainted after returning if it's tainted in any execution,
      instead of any storage
    - Changed fake-vulnerable test cases to account for this

--- project/test_contracts/functions/infinite-recursion-fake-vulnerable-global.sol
+++ project/test_contracts/functions/infinite-recursion-fake-vulnerable-global.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.5.0;
 contract Contract {
   address payable owner;
   function foo() public {
-    owner = owner;
+    owner = msg.sender;  // tainted
+    owner = address(0xDEADBEEF);  // clean
     foo();
     selfdestruct(owner);  // safe
   }
