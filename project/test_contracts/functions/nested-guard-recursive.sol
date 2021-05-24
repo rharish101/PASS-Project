ee561673c9ab0c82f10eaeaa4ad56c33325c3508
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-05-24 19:22:51 +0200

    Made recursive test case bounded

--- project/test_contracts/functions/nested-guard-recursive.sol
+++ project/test_contracts/functions/nested-guard-recursive.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.5.0;
 // the output of your analyzer should be Tainted
 contract Contract {
   address payable owner;
-  function foo() public {
-    require(a());
+  function foo(int x) public {
+    require(a(x));
     selfdestruct(msg.sender);  // safe
   }
 
-  function a() public returns(bool) {
-    return msg.sender == owner || a(); // guard
+  function a(int x) public returns(bool) {
+    if (x > 3) {
+      return a(x - 1) || true;
+    } else {
+      return msg.sender == owner; // guard
+    }
   }
 }
