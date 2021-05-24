b7aa819a7377e1c9c43228ef6b4b95beaee659ec
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-05-24 19:23:09 +0200

    Added test cases for recursive notguard

---
+++ project/test_contracts/functions/nested-notguard-recursive.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+// the contract is vulnerable
+// the output of your analyzer should be Tainted
+contract Contract {
+  address payable owner;
+  function foo(int x) public {
+    require(a(x));
+    selfdestruct(msg.sender);  // vulnerable
+  }
+
+  function a(int x) public returns(bool) {
+    if (x > 3) {
+      return a(x - 1) || true;
+    } else {
+      return true; // not guard
+    }
+  }
+}
